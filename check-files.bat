fdb3786491909f71ce4772d8b3c86a71bcbd76f4
Peter Grainger <peter@grainger.xyz>
2019-10-25 16:28:04 +0100

    Update check-files.bat

--- check-files.bat
+++ check-files.bat
@@ -1,3 +1,1 @@
-dir C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE
-dir C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\
-dir C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\
+dir C:\Program Files (x86)\
