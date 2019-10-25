ef6c12c77c96d65cca064a830cbd5cf36f0e522e
Peter Grainger <peter@grainger.xyz>
2019-10-25 16:23:35 +0100

    Create check-files.bat

---
+++ check-files.bat
@@ -0,0 +1,3 @@
+dir C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE
+dir C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\
+dir C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\
