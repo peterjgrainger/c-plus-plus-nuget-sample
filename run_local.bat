f2427fd1167ab2efb647804aecc983afb0063d85
Peter Grainger <Peter.Grainger@arup.com>
2019-10-27 13:10:22 +0000

    run local works

---
+++ run_local.bat
@@ -0,0 +1,16 @@
+REM "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE\devenv.com" Project1.sln /build Debug /project Project1.vcxproj
+REM "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE\devenv.com" Project1.sln /build Release /project Project1.vcxproj
+
+mkdir package\build\native\include\Project1
+copy *.h package\build\native\include\Project1
+
+mkdir package\build\native\lib\static\v141\x64\Release
+mkdir package\build\native\lib\static\v141\x64\Debug
+
+copy x64\Debug\Project1.lib package\build\native\lib\static\v141\x64\Debug
+copy x64\Debug\Project1.pdb package\build\native\lib\static\v141\x64\Debug
+
+copy x64\Release\Project1.lib package\build\native\lib\static\v141\x64\Release
+
+
+
