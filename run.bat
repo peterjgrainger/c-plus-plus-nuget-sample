a31eb20dd79b26da9be0a91d9a2772a5f621d63e
Peter Grainger <peter@grainger.xyz>
2019-10-27 11:46:27 +0000

    local debug

--- run.bat
+++ run.bat
@@ -2,15 +2,14 @@
 "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\devenv.com" Project1.sln /build Release /project Project1.vcxproj
 
 mkdir package\build\native\include\Project1
-copy ./*.h package\build\native\include\Project1
+copy *.h package\build\native\include\Project1
 
 mkdir package\build\native\lib\static\v141\x64\Release
 mkdir package\build\native\lib\static\v141\x64\Debug
 
-copy ..\x64\Debug\Project1.lib package\build\native\lib\static\v141\x64\Debug
-copy ..\x64\Debug\Project1.pdb package\build\native\lib\static\v141\x64\Debug
+copy x64\Debug\Project1.lib package\build\native\lib\static\v141\x64\Debug
+copy x64\Debug\Project1.pdb package\build\native\lib\static\v141\x64\Debug
 
-copy ..\x64\Release\Project1.lib package\build\native\lib\static\v141\x64\Release
-
+copy x64\Release\Project1.lib package\build\native\lib\static\v141\x64\Release
 
 
